097a831c520b215df6ebbe25d93608a5cf5c33f3
NavanMapp <navanmapp@gmail.com>
2022-11-28 10:42:31 -0800

    python

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,12 @@
+#include "list.h"
+
+/**
+  * check_cycle - function that checks if a singly linked list has cycle in it
+  * @list: the linked list
+  *
+  * Return: 0 if no cycle, 1 if there is a cycle
+  */
+
+int check_cycle(listint_t *list)
+{
+
