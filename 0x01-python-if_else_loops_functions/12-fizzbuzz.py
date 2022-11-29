c966ac7430a62ea7e8b9eb6c5fc03c67f3b9e6b2
Návan <95779113+NavanMapp@users.noreply.github.com>
2022-11-29 21:36:24 +0200

    python

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,8 +3,8 @@ def fizzbuzz():
 	for i in range(100):
 		if i % 3 == 0 and i % 5 == 0:
 			print("FizzBuzz", end=' ')
-			elif i % 3 == 0:
-				print("Fizz", end=' ')
-				elif i % 5 == 0:
-					print("Buzz", end=' ')
-					else: print(i, end=' ')
+		elif i % 3 == 0:
+			print("Fizz", end=' ')
+		elif i % 5 == 0:
+			print("Buzz", end=' ')
+		else: print(i, end=' ')
