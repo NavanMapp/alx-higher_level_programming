59ba3eb0ed24e8686afed9523d289083d153deaa
NavanMapp <navanmapp@gmail.com>
2022-11-29 07:30:40 -0800

    python

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,2 +1,2 @@
 #!/usr/bin/python3
-for alphabet in
+for alphabet in 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','w','x','y','z':
---
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+for number in range(98):
+	number += 1
+	print(f'{number} = ', hex(number))
+
