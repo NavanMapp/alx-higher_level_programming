ef7ac1bb55ee59c116edd69b94edea92e6655e1c
Návan <95779113+NavanMapp@users.noreply.github.com>
2022-11-29 22:25:24 +0200

    Update 2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for alphabet in 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','w','x','y','z':
-	print(alphabet, end='')
+    print('{}'.format(alphabet), end='')
