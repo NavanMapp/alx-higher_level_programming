efe54434b88d4a0236cd17d19a304bbe69ac0924
NavanMapp <navanmapp@gmail.com>
2022-11-29 11:27:26 -0800

    python

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,13 @@
+#include "list.h"
+
+/**
+  * insert_node - function that inserts a number into a sorted singly linked list
+  * @head: start of a list
+  * @number: number inserted into list
+  * Return: NULL
+  */
+
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *new;
+	list
---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def uppercase(str):
+	for string in str:
+		if (ord(str[i]) >= 65 and ord(str[i]) <= 90):
+			print(str)
