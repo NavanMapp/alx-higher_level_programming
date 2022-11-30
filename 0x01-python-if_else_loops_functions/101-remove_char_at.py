3ab31cb706d6399eca8d497fe15ae8f23d029eb8
Návan <95779113+NavanMapp@users.noreply.github.com>
2022-11-30 19:46:31 +0200

    Update 101-remove_char_at.py

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-	new_char = ''
-	str = str[:n] + new_char + str[n+1:]
-	return str
+    new_char = ''
+    str = str[:n] + new_char + str[n+1:]
+    return (str)
