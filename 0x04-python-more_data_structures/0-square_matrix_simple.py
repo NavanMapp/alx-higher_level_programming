4d0e67d27a6e116078aa3a6ccca23c04e28e43b0
NavanMapp <navanmapp@gmail.com>
2022-12-07 21:29:47 +0000

    0x04-python-more_data_structures

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    if not matrix:
-        print()
-    return [[item**2 for item in row] for row in matrix]
+    return ([list(map(lambda x: x * x, row)) for row in matrix])
