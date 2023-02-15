6407d061b46a7596b7b7d4671ff7e072272247f7
Návan <95779113+NavanMapp@users.noreply.github.com>
2023-02-15 18:10:22 +0200

    0x0E-SQL_more_queries

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,2 +1,9 @@
 -- Write a script that lists all genres in the database hbtn_0d_tvshows_rate by their rating.
 -- Import the database dump from hbtn_0d_tvshows_rate to your MySQL server: download (same as 102-rating_shows.sql)
+SELECT g.`name` AS `genre`,
+       COUNT(*) AS `number_of_shows`
+  FROM `tv_genres` AS g
+       INNER JOIN `tv_show_genres` AS t
+       ON g.`id` = t.`genre_id`
+ GROUP BY g.`name`
+ ORDER BY `number_of_shows` DESC;
