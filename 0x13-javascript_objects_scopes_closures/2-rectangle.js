736cd04dc20992e2a65e066177520e49de6429bd
NavanMapp <navanmapp@gmail.com>
2023-03-14 15:10:39 +0000

    0x13-javascript_objects_scopes_closures

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// Write a class Rectangle that defines a rectangle
+class Rectangle {
+    constructor(w, h) {
+        this.height = h;
+        this.width = w;
+    }
+
+    if (w && h === 0){
+        const rec = new();
+    }
+}
