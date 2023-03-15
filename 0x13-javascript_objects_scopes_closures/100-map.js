8ded626e463816ec84a3f01b564ff00483ce5f33
NavanMapp <navanmapp@gmail.com>
2023-03-15 17:12:16 +0000

    0x13-javascript_objects_scopes_closures

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -10,3 +10,5 @@ Print both the initial list and the new list
 const list = require('./100-data.js').list;
 console.log(list);
 console.log(list.map((item, index) => item * index));
+
+exports.list = [1, 2, 3, 4, 5];
