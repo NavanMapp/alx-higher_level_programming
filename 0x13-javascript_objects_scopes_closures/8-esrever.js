f26fafcc8a6be3e358125844e34133b0b7d8dee8
Návan <95779113+NavanMapp@users.noreply.github.com>
2023-03-15 19:14:17 +0200

    0x13-javascript_objects_scopes_closures

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -6,8 +6,3 @@ exports.esrever = function(list){
         return array;
     }, []);
 };
-
-const esrever = require('./8-esrever').esrever;
-
-console.log(esrever([1, 2, 3, 4, 5]));
-console.log(esrever(["School", 89, { id: 12 }, "String"]));
