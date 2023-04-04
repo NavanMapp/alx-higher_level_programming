e36e9a81f67be13502c439a61d71380bb368f4b9
Návan <95779113+NavanMapp@users.noreply.github.com>
2023-04-04 20:32:23 +0200

    0x11-python-network_1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """Python script that fetches https://intranet.hbtn.io/status."""
 if __name__ == "__main__":
-  
     import urllib.request
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
         content = res.read()
